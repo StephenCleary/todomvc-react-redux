408ecf2c2d5f82b04a284dfaaaa6396f14f0bd42
Stephen Cleary <github.ourteddybear@xoxy.net>
2016-02-11 21:15:52 -0500

    Added Redux immutable state invariant.

--- js/store.jsx
+++ js/store.jsx
@@ -1,6 +1,12 @@
-import { createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+import immutableState from 'redux-immutable-state-invariant';
 import reducers from './reducers';
 import DevTools from './devTools';
 
+// NOTE FOR PRODUCTION: the Redux immutable state invariant should not be used in production apps!
+const middleware = applyMiddleware(immutableState());
+
 // NOTE FOR PRODUCTION: DevTools should not be used in production apps!
-export default createStore(reducers, DevTools.instrument());
+const storeFactory = compose(middleware, DevTools.instrument())(createStore);
+
+export default storeFactory(reducers);
