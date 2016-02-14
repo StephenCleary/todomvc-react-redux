e001da5a000f207089c5835c97ddb3fa6b8dd98c
Stephen Cleary <github.ourteddybear@xoxy.net>
2016-02-14 16:58:34 -0500

    Split out TodoList.

--- js/components/main.jsx
+++ js/components/main.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import TodoList from './todoList';
 
 export default function Main() {
     // This section should be hidden by default and shown when there are todos
@@ -6,26 +7,7 @@ export default function Main() {
         <section className="main">
             <input className="toggle-all" type="checkbox"/>
             <label htmlFor="toggle-all">Mark all as complete</label>
-            <ul className="todo-list">
-                {/* These are here just to show the structure of the list items */}
-                {/* List items should get the class `editing` when editing and `completed` when marked as completed */}
-                <li className="completed">
-                    <div className="view">
-                        <input className="toggle" type="checkbox" checked/>
-                        <label>Taste JavaScript</label>
-                        <button className="destroy"/>
-                    </div>
-                    <input className="edit" value="Create a TodoMVC template"/>
-                </li>
-                <li>
-                    <div className="view">
-                        <input className="toggle" type="checkbox"/>
-                        <label>Buy a unicorn</label>
-                        <button className="destroy"/>
-                    </div>
-                    <input className="edit" value="Rule the web"/>
-                </li>
-            </ul>
+            <TodoList/>
         </section>
     );
 }
